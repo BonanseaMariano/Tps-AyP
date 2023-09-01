e862703270c6e32e2d42957f53074069424dbca2
Mariano Bonansea <marianobonanseapetrovial@gmail.com>
2023-09-01 15:04:03 -0300

    Avances

--- Tp2/Ejercicio7Tp2.c
+++ Tp2/Ejercicio7Tp2.c
@@ -1,24 +1,17 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(){
-    char texto[10][20];
-    char inp[20];
+int posAlf(char input);
 
-    //Limpieza del arreglo
-    for (int i = 0; i < 10; i++)
-    {
-        for (int j = 0; i < j; j++)
-        {
-            texto[i][j]=NULL;
-        }
-    }
-    
+int main(){
+    char texto[10][20]="";
+    char inp[20];
 
     do
     {
+        printf("Ingrese la cadena:\n");
         scanf("%[^\n]s",inp);
-
+        posAlf(inp);
         
     } while (strcmp(texto,"fin")!=0);
     
@@ -26,4 +19,14 @@ int main(){
 
 
     return 0;
+}
+
+int posAlf(char input){
+    //Recorro el arreglo hasta encontrar la posicion en la que colocar la nueva cadena
+    for (int i = 0; i < 10; i++)
+    {
+        /* code */
+    }
+    
+
 }
--- Tp2/Ejercicio9Tp2.c
+++ Tp2/Ejercicio9Tp2.c
@@ -1,27 +1,63 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
+
+bool ingreso(char *inp,char a[20][30]);
+void imprimir(int cont,char a[20][30]);
 
 int main(){
-    char arreglo[20][30];
-    char inp[50];
-    
+    char arreglo[20][30];    
+    int cont=0;
+
     do
     {
-        printf("Ingresa la cadena de max 30 caracteres\n");
-        scanf(" %[^\n]s",inp);
-    } while (strlen(inp)>30);
-    strcpy(arreglo[0],inp);
-
-    for (int i = 1; i < 5; i++)
-    {
+        //Long max 30
+        char inp[50]="";
         do
         {
             printf("Ingresa la cadena de max 30 caracteres\n");
             scanf(" %[^\n]s",inp);
-            //Evaluar letras
         } while (strlen(inp)>30);
+        if (ingreso(&inp,&arreglo[20][30]))
+        {
+            strcpy(arreglo[0],inp);
+            cont++;
+            imprimir(cont,arreglo[20][30]);
+        }
+    } while (cont<20);
+    
+
+    return 0;
+}
+
+bool ingreso(char *inp,char a[20][30]){
+    int maylen=-1;
+    
+    //For de recorrer arreglo (filas)
+    for (int i = 0; i < 20; i++)
+    {
+        //Evaluo la cadena mas larga para poder usar eso en el for
+        if (strlen(inp)>strlen(a[i][0]))
+        {
+            maylen=strlen(inp);
+        }else{
+            maylen=strlen(a[i][0]);
+        }
+        //Contador de letras iguales
+        int letig=0;
+        //For de recorrer letra por letra
+        for (int j = 0; j <= maylen; j++)
+        {
+            if
+        }
         
     }
     
-    return 0;
+}
+
+void imprimir(int cont,char a[20][30]){
+    for (int i = 0; i <= cont; i++)
+    {
+        printf("%s\n",a[i][0]);
+    }
 }
