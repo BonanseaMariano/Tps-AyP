9407ae1c8a4d1a737fa30ec498023e56fff78f21
Mariano Bonansea <marianobonanseapetrovial@gmail.com>
2023-09-15 21:07:01 -0300

    Correccion

--- Tp2/Ejercicio2.c
+++ Tp2/Ejercicio2.c
@@ -9,11 +9,13 @@ int aleatorio_en_rango(int minimo, int maximo);
 
 int main(){
     int arreglo[10],i=0;
+    int frecuencia[10]={0,0,0,0,0,0,0,0,0,0};
     srand(getpid()); 
     //Carga de numeros random sin repetir
     //Iterador de recorrida del arreglo
     while (i < 10) {
         int numero = rand() % 11;
+        frecuencia[numero]++;
         int j = 0;
         //Iterador de control
         while (j < i && arreglo[j] != numero) {
@@ -28,6 +30,12 @@ int main(){
     for (i = 0; i < 10; i++)
     {
         printf("%i\t",arreglo[i]);
+    }
+    printf("\n\t---- FRECUENCIAS ----\n");
+    //Impresion frecuencia
+    for (i = 0; i < 10; i++)
+    {
+        printf("Numero %i = %i veces\n",i,frecuencia[i]);
     }
     return 0;
 }
