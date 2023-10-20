5865eab75edaac1ad8668b239dfa2dd958595253
Mariano Bonansea <marianobonanseapetrovial@gmail.com>
2023-10-20 17:53:53 -0300

    Ejercicio3_Tp5

---
+++ Tp5/Ejercicio3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(){
+    FILE *archivo;
+    char c;
+    pid_t pid = getpid();
+    archivo = fopen("pidfile","r");
+    if (archivo == NULL)
+    {
+        archivo = fopen("pidfile","w");
+        fwrite(&pid,sizeof(pid_t),1,archivo);
+        printf("%d\n",getpid());
+        fclose(archivo);
+        getchar();
+        if(remove("pidfile")!=0) printf("Error al eliminar el archivo");
+        return 0;
+    }
+    fclose(archivo);
+    printf("El programa ya se encuentra en ejecución");
+    return 0;
+}
