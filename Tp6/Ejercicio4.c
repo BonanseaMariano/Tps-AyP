4f7f510f480426971e561a37fb0f232e613d35c5
Mariano Bonansea <marianobonanseapetrovial@gmail.com>
2023-10-26 10:09:57 -0300

    Ahora si la piramide bn

--- Tp6/Ejercicio4.c
+++ Tp6/Ejercicio4.c
@@ -1,22 +1,26 @@
 #include <stdio.h>
 
-void imprimir_piramide(int n);
+void imprimir_piramide(int n,int i);
 
 int main() {
     int n;
     printf("Ingrese un numero natural positivo: ");
     scanf("%d", &n);
-    imprimir_piramide(n);
+    imprimir_piramide(n,1);
     return 0;
 }
 
-void imprimir_piramide(int n) {
+void imprimir_piramide(int n,int i) {
     if (n == 0) {
         return;
     }
     for (int j = 0; j < n; j++) {
-        printf("%d ", n);
+        printf(" ");
     }
-    printf("\n");  
-    imprimir_piramide(n-1);
+    for (int k = i; k > 0 ; k--)
+    {
+        printf("%d ",i);
+    }
+    printf("\n");
+    imprimir_piramide(n-1,++i);
 }
